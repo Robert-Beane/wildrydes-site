295ec943ad32bb57ce764d885b44eedc3202747e
Robert Beane <54686477+Robert-Beane@users.noreply.github.com>
2021-03-31 11:58:59 -0500

    Update config.js
    Add my own cognito ID's

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_NHM2B0c8z', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '202tcvo9rmgo1qudb4d0p66gqv', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_n3m6fIZ8V', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '5hn8qguhivu89j35qcd47b5hil', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
